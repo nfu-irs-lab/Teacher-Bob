7d4d6e3025f5c95e3527130fbac6bb76767bf7a1
VincentYeh-dev <vincent3314534>
2022-10-17 22:23:32 +0800

    完成基本Http Server.

--- web/httpserver.js
+++ web/httpserver.js
@@ -1,13 +1,26 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const http_1 = require("http");
+const express_1 = __importDefault(require("express"));
 const port = 80;
-const server = (0, http_1.createServer)((request, response) => {
-    response.on('error', (err) => {
-        console.error(err);
-    });
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end('Hello world!');
+const app = (0, express_1.default)();
+app.get('/about-me', (request, response) => {
+    response.type('text/plain');
+    response.send('My name is Jimmy.');
 });
-server.listen(port);
-console.log(`server is running on http://localhost:80`);
+// public資料夾的檔案會被顯示在網頁上
+app.use(express_1.default.static(__dirname + '/public'));
+//  **很重要**
+//  一定要在全部express.use或get之後，否則會永遠顯示找不到網頁
+app.use((request, response) => {
+    response.type('text/plain');
+    response.status(404);
+    response.send('找不到網頁');
+});
+// --------------------
+// 開啟伺服器
+app.listen(port, () => {
+    console.log(`server is running on http://localhost:80`);
+});

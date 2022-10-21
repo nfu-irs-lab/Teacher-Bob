f44cba94a6b029057e5a3614a0a020426eee8b22
VincentYeh-dev <vincent3314534>
2022-10-21 17:30:49 +0800

    增加dynamixel程式

---
+++ python/main.py
@@ -0,0 +1,28 @@
+import socket
+
+bind_ip = "0.0.0.0"
+bind_port = 4444
+EOP = bytes([0xe2, 0x80, 0xA9]).decode('utf-8')
+
+if __name__ == "__main__":
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((bind_ip, bind_port))
+    server.listen(5)
+
+    while True:
+        client, address = server.accept()
+        print("Connected:", address)
+        try:
+            while True:
+                data = client.recv(1024)
+                print("receive:")
+                print(data)
+
+                strs=data.decode(encoding='utf-8')
+
+                if strs.__contains__(EOP):
+                    print(strs.index(strs))
+                client.send("ACK!".encode('utf-8'))
+
+        except Exception as e:
+            print(e.__str__())

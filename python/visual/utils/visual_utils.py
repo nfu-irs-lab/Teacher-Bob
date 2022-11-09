9f932f2be2f1671c7f021678de353d1dadd9bc67
VincentYeh-dev <vincent3314534>
2022-11-09 19:36:35 +0800

    完成影像辨識主程式

--- python/visual/utils/visual_utils.py
+++ python/visual/utils/visual_utils.py
@@ -3,18 +3,13 @@ from typing import Optional
 import cv2
 
 
-def annotateLabel(image, x, y, width, height, label, overwrite: bool = True) -> Optional:
-    if not overwrite:
-        result = image.copy()
-    else:
-        result = image
+def annotateLabel(image, x, y, width, height, label) -> Optional:
+    result = image
     cv2.rectangle(result, (x, y), (x + width, y + height),
                   (0, 255, 0), 2)
     font = cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(result, label,
                 (x, y - 10), font,
                 1, (0, 0, 255), 2, cv2.LINE_4)
-    if not overwrite:
-        return result
-    else:
-        return None
+
+    return result

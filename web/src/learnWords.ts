785a7672a9237df675145b56e49cf2e61665ccc1
cc0931135451 <yoyochaning@gmail.com>
2022-11-18 21:33:52 +0800

    1.確認單字庫總單字為52組 2.能利用亂數的方式隨機抓取vocabularies.json裏的id並印到html上

--- web/src/learnWords.ts
+++ web/src/learnWords.ts
@@ -1,6 +1,12 @@
 function GoTolearnWord(){
-  open('http://127.0.0.1:5500/public/learnWords.html')
+  open('http://127.0.0.1:5500/public/learnWords.html')//開啟learnWord的網址
 }
 
 let grabWord : number
-grabWord = Math.floor(Math.random()*73)//利用亂數抓單字庫的單字
+grabWord = Math.floor(Math.random()*51)//利用亂數抓單字庫的單字
+//目前單字庫有52組單字，如有新增記得改()後面的數字與此註記
+
+console.log(grabWord)
+fetch("./resourse/vocabularies.json") 
+  .then((resourse) => resourse.json())
+  .then((json) => console.log(json[grabWord].id));//將vocabularies.json檔導入learnWord.ts

7f4394e7fea83e0a78b7888b44d7b909f35cdd67
ZhengKungFu <Nfu202208@gmail.com>
2022-11-21 21:50:09 +0800

    得到inputstory的數字了

--- web/public/StoryTelling.js
+++ web/public/StoryTelling.js
@@ -3,34 +3,56 @@ var _a, _b;
 function GoStorySelectPage() {
     open("http://127.0.0.1:5500/public/StorySelectPage.html");
 }
-// 獲取使用者選擇的故事
-function GetInputStory() {
+let StoryNum;
+let PageCount = 0;
+let EngSubState = false;
+//透過fetch在後台服務器獲取數據，透過第一個then將原始數據轉換成.json格式
+const GetDataFromJson = fetch("http://127.0.0.1:5500/public/resourse/story.json").then((response) => response.json());
+function PromiseToString(promise) {
+    let tem = JSON.stringify(promise);
+    let result = JSON.parse(tem);
+    return result;
+}
+function GetInputStoryNum() {
     const InputStory = document.getElementById("Storylist");
-    if (typeof InputStory != null) {
-        InitializePage(InputStory);
+    StoryNum = Number(InputStory === null || InputStory === void 0 ? void 0 : InputStory.value.slice(5));
+    console.log("story" + StoryNum);
+    return StoryNum;
+}
+function InitializePage() {
+    var _a, _b;
+    (_a = document.getElementById("FirstPage")) === null || _a === void 0 ? void 0 : _a.setAttribute("style", "display:none");
+    (_b = document.getElementById("SecondPage")) === null || _b === void 0 ? void 0 : _b.setAttribute("style", "display:block");
+}
+function ChangeState() {
+    var _a, _b;
+    if (EngSubState == false) {
+        (_a = document
+            .getElementById("EnglishSubtitle")) === null || _a === void 0 ? void 0 : _a.setAttribute("style", "display:block");
+        EngSubState = true;
     }
-    else
-        alert("找不到故事");
+    else {
+        (_b = document
+            .getElementById("EnglishSubtitle")) === null || _b === void 0 ? void 0 : _b.setAttribute("style", "display:none");
+        EngSubState = false;
+    }
 }
-function InitializePage(TitleName) {
-    // 顯示故事中英文標題
-    const title = document.getElementById("title");
-    if (title != null)
-        title.innerHTML = TitleName.value + "<br>";
-    console.log(TitleName.value);
-    let obj = JSON.parse('{"text":"string"}');
-    console.log(obj.text);
-}
-// const mode = require("./StoryLib/text.json");
-// for (let i = 0; i < mode.length; i++) {
-//   console.log(mode[i]);
+let btn1 = document.getElementById("EnglishSubtitle");
+if (btn1 != null)
+    btn1.onclick = ChangeState;
+let CheckStoryNum = () => {
+    GetDataFromJson.then(function (json) {
+        for (let i = 0; i < json.length; i++) {
+            if (json[i].id == `story${i + 1}`) {
+                return i;
+            }
+        }
+    });
+};
+// async function GetStoryNumber() {
+//   let storynum = await GetDataFromJson;
 // }
-fetch("./resourse/story.json")
-    .then((response) => response.json())
-    .then((json) => console.log(json[0].id));
-// document
-//   .getElementById(InputStory?.value as string)
-//   ?.addEventListener("click", () => {
+// document.getElementById(InputStory?.value as string)?.addEventListener("click", () => {
 //     console.log(InputStory?.value);
 //     // 關閉故事選擇介面
 //     let StoryList = document.getElementById("storylist");
--- web/src/StoryTelling.ts
+++ web/src/StoryTelling.ts
@@ -1,59 +1,60 @@
 function GoStorySelectPage() {
   open("http://127.0.0.1:5500/public/StorySelectPage.html");
 }
-// 獲取使用者選擇的故事
-function GetInputStory() {
+let StoryNum: number;
+let PageCount: number = 0;
+let EngSubState: boolean = false;
+//透過fetch在後台服務器獲取數據，透過第一個then將原始數據轉換成.json格式
+const GetDataFromJson = fetch(
+  "http://127.0.0.1:5500/public/resourse/story.json"
+).then((response) => response.json());
+
+function PromiseToString(promise: Promise<any>): String {
+  let tem = JSON.stringify(promise);
+  let result = JSON.parse(tem);
+  return result;
+}
+
+function GetInputStoryNum(): number {
   const InputStory = document.getElementById(
     "Storylist"
   ) as HTMLInputElement | null;
-  if (typeof InputStory != null) {
-    InitializePage(InputStory as HTMLInputElement);
-  } else alert("找不到故事");
+  StoryNum = Number(InputStory?.value.slice(5));
+  console.log("story" + StoryNum);
+  return StoryNum;
 }
-
-function InitializePage(TitleName: HTMLInputElement) {
-  // 顯示故事中英文標題
-  const title = document.getElementById("title");
-  if (title != null) title.innerHTML = TitleName.value + "<br>";
-  console.log(TitleName.value);
-  let obj: namespace.Page = JSON.parse('{"text":"string"}');
-
-  console.log(obj.text);
+function InitializePage() {
+  document.getElementById("FirstPage")?.setAttribute("style", "display:none");
+  document.getElementById("SecondPage")?.setAttribute("style", "display:block");
 }
-
-// const mode = require("./StoryLib/text.json");
-// for (let i = 0; i < mode.length; i++) {
-//   console.log(mode[i]);
+function ChangeState() {
+  if (EngSubState == false) {
+    document
+      .getElementById("EnglishSubtitle")
+      ?.setAttribute("style", "display:block");
+    EngSubState = true;
+  } else {
+    document
+      .getElementById("EnglishSubtitle")
+      ?.setAttribute("style", "display:none");
+    EngSubState = false;
+  }
+}
+let btn1 = document.getElementById("EnglishSubtitle");
+if (btn1 != null) btn1.onclick = ChangeState;
+let CheckStoryNum = () => {
+  GetDataFromJson.then(function (json) {
+    for (let i = 0; i < json.length; i++) {
+      if (json[i].id == `story${i + 1}`) {
+        return i;
+      }
+    }
+  });
+};
+// async function GetStoryNumber() {
+//   let storynum = await GetDataFromJson;
 // }
-fetch("./resourse/story.json")
-  .then((response) => response.json())
-  .then((json) => console.log(json[0].id));
-
-declare module namespace {
-  export interface Page {
-    text: string;
-    audio: string;
-    image: string;
-    action: string;
-  }
-
-  export interface Data {
-    name: string;
-    total: number;
-    pages: Page[];
-  }
-
-  export interface RootObject {
-    id: string;
-    name: string;
-    chinesesubtitle: string;
-    data: Data;
-  }
-}
-
-// document
-//   .getElementById(InputStory?.value as string)
-//   ?.addEventListener("click", () => {
+// document.getElementById(InputStory?.value as string)?.addEventListener("click", () => {
 //     console.log(InputStory?.value);
 //     // 關閉故事選擇介面
 //     let StoryList = document.getElementById("storylist");
@@ -70,8 +71,8 @@ declare module namespace {
 
 //     // open("./StoryLib/story1.txt");
 //   });
-document.getElementById("text")?.addEventListener("click", () => {});
 
+document.getElementById("text")?.addEventListener("click", () => {});
 const synth = window.speechSynthesis;
 const utter = new SpeechSynthesisUtterance();
 utter.text = document.getElementById("function5")?.innerText as string;

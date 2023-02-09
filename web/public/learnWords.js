c56ade1bd07b298273c8fc3749a3b0dac306b5be
cc0931135451 <yoyochaning@gmail.com>
2023-02-09 22:37:30 +0800

    解決 cannot read properties of undefined就可以完成功能

--- web/public/learnWords.js
+++ web/public/learnWords.js
@@ -3,15 +3,55 @@ function goToLearnWord() {
     open('http://127.0.0.1:5500/public/learnWords.html'); //開啟learnWord的網址
 }
 let grabWord;
+let StoryNum1;
+const GetDataFromJson1 = fetch("http://127.0.0.1:5500/public/resourse/vocabularies.json").then((response) => response.json());
+function GetInputStory1() {
+    const InputStory = document.getElementById("Storylist");
+    StoryNum1 = Number(InputStory === null || InputStory === void 0 ? void 0 : InputStory.value.slice(5));
+    return StoryNum1 - 1;
+}
+let EngSubState1 = false;
+function ChangeState1() {
+    var _a, _b;
+    if (EngSubState1 == false) {
+        (_a = document
+            .getElementById("EnglishSubtitle")) === null || _a === void 0 ? void 0 : _a.setAttribute("style", "display:block");
+        EngSubState1 = true;
+    }
+    else {
+        (_b = document
+            .getElementById("EnglishSubtitle")) === null || _b === void 0 ? void 0 : _b.setAttribute("style", "display:none");
+        EngSubState1 = false;
+    }
+}
 grabWord = Math.floor(Math.random() * 51); //利用亂數抓單字庫的單字
 //目前單字庫有52組單字，如有新增記得改()後面的數字與此註記
 console.log(grabWord);
-//將vocabularies.json檔導入learnWord.ts
-let a;
-fetch("./resourse/vocabularies.json")
-    .then((resourse) => resourse.json())
-    .then((json) => a = (json[grabWord].data.name))
-    .then((a) => console.log(a));
+function StringfyJson1(JsonWords) {
+    let StringfyJsonWord = JSON.stringify(JsonWords);
+    let ParseJsonWord = JSON.parse(StringfyJsonWord);
+    return ParseJsonWord;
+}
+let CurrentPageNumber1 = 0;
+function ShowLearnWord() {
+    let EnglishSubtitleShow = document.getElementById("EnglishSubtitle");
+    if ((EnglishSubtitleShow === null || EnglishSubtitleShow === void 0 ? void 0 : EnglishSubtitleShow.style.display) == "none") {
+        EnglishSubtitleShow.setAttribute("style", "display:block");
+        let Showbutton = document.getElementById("ShowButton");
+        Showbutton === null || Showbutton === void 0 ? void 0 : Showbutton.addEventListener("click", function () {
+            let grabWord = GetInputStory1();
+            GetDataFromJson1.then(function (json) {
+                StringfyJson1(json[grabWord].data.languages[CurrentPageNumber1].tr_name.text);
+                let Target = document.getElementById("EnglishSubtitle");
+                Target.innerHTML = StringfyJson1(json[grabWord].data.languages[CurrentPageNumber1].tr_name.text);
+            });
+        });
+    }
+    else {
+        EnglishSubtitleShow === null || EnglishSubtitleShow === void 0 ? void 0 : EnglishSubtitleShow.setAttribute("style", "display:none");
+    }
+}
+console.log(StringfyJson1);
 // fetch("./resourse/vocabularies.json") 
 // .then((resourse) => resourse.json())
 // .then((json) => console.log(json[grabWord].data.sentence));
--- web/src/learnWords.ts
+++ web/src/learnWords.ts
@@ -3,17 +3,66 @@ function goToLearnWord(){
 }
 
 let grabWord : number
+let StoryNum1: number;
+
+const GetDataFromJson1 = fetch(
+  "http://127.0.0.1:5500/public/resourse/vocabularies.json"
+).then((response) => response.json());
+
+function GetInputStory1(): number {
+  const InputStory = document.getElementById(
+    "Storylist"
+  ) as HTMLInputElement | null;
+  StoryNum1 = Number(InputStory?.value.slice(5));
+  return StoryNum1 - 1;
+}
+
+let EngSubState1: boolean = false;
+function ChangeState1() {
+  if (EngSubState1 == false) {
+    document
+      .getElementById("EnglishSubtitle")
+      ?.setAttribute("style", "display:block");
+    EngSubState1 = true;
+  } else {
+    document
+      .getElementById("EnglishSubtitle")
+      ?.setAttribute("style", "display:none");
+    EngSubState1 = false;
+  }
+}
 grabWord = Math.floor(Math.random()*51)//利用亂數抓單字庫的單字
 //目前單字庫有52組單字，如有新增記得改()後面的數字與此註記
-
 console.log(grabWord)
-//將vocabularies.json檔導入learnWord.ts
 
-let a : string
-fetch("./resourse/vocabularies.json") 
-.then((resourse) => resourse.json())
-.then((json) =>  a = (json[grabWord].data.name))
-.then((a) => console.log(a))
+function StringfyJson1(JsonWords: string): string {
+  let StringfyJsonWord = JSON.stringify(JsonWords);
+  let ParseJsonWord: string = JSON.parse(StringfyJsonWord);
+  return ParseJsonWord;
+}
+
+let CurrentPageNumber1: number = 0;
+function ShowLearnWord() {
+  let EnglishSubtitleShow = document.getElementById("EnglishSubtitle");
+  if (EnglishSubtitleShow?.style.display == "none") {
+    EnglishSubtitleShow.setAttribute("style", "display:block");
+    let Showbutton = document.getElementById("ShowButton");
+    Showbutton?.addEventListener("click", function () {
+      let grabWord = GetInputStory1();
+      GetDataFromJson1.then(function (json) {
+        StringfyJson1(json[grabWord].data.languages[CurrentPageNumber1].tr_name.text);
+        let Target = document.getElementById("EnglishSubtitle");
+        Target!.innerHTML = StringfyJson1(
+          json[grabWord].data.languages[CurrentPageNumber1].tr_name.text
+        );
+      });
+    });
+  } else {
+    EnglishSubtitleShow?.setAttribute("style", "display:none");
+  }
+}
+
+console.log(StringfyJson1)
 
 
   // fetch("./resourse/vocabularies.json") 
